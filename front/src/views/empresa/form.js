f8eb9eeea6c219e7bd2a7a81b9ef1f4e5bf87b8b
Nayton Batista <naytonbatista@gmail.com>
2019-09-04 23:09:33 -0300

    Adição do form de empresa

--- front/src/views/empresa/form.js
+++ front/src/views/empresa/form.js
@@ -1,6 +1,29 @@
 import React from 'react'
 import { Formik, Form, Field } from 'formik'
 
+const modelo = [{
+    linha: [
+        { descricao: 'Razão Social', placeholder: 'Razão Social', type: "text", name: 'razao_social', width: '7' },
+        { descricao: 'CNPJ', placeholder: 'CNPJ', type: "text", name: 'cnpj', width: '3' }
+    ]
+},
+{
+    linha: [
+        { descricao: 'Inscrição Estadual', placeholder: 'Inscrição Estadual', type: "text", name: 'inscricao_estadual', width: '3' },
+        { descricao: 'Home Page', placeholder: 'Home Page', type: "text", name: 'homepage', width: '5' },
+        { descricao: 'Qtde Empregados', placeholder: 'Qtde Empregados', type: "text", name: 'quantidade_empregados', width: '2' },
+    ]
+},
+{
+    linha: [
+        { descricao: 'Sócio Administrador', placeholder: 'Sócio Admnistrador', type: "text", name: 'socio_administrador', width: '5' },
+        { descricao: 'Sede', placeholder: 'Sede', type: "text", name: 'sede', width: '5' },
+
+    ]
+}
+
+];
+
 const FormEmpresa = () => {
 
     return (
@@ -9,33 +32,29 @@ const FormEmpresa = () => {
             <Formik>
                 <Form>
                     <div className="container">
-                        <div className="row">
-                            <div className="col-sm-7">
-                                <div className="form-group">
-                                    <label>Razão Social</label>
-                                    <Field className="form-control" placeholder="Razão social" type="text" name="razao_social" />
-                                </div>
-                            </div>
-                            <div className="col-sm-3">
-                                <div className="form-group">
-                                    <label>CNPJ</label>
-                                    <Field className="form-control" placeholder="CNPJ" type="text" name="nome_fantasia" />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-sm-7">
-                                <div className="form-group">
-                                    <label>Inscrição Estadual</label>
-                                    <Field className="form-control" placeholder="Inscrição Estadual" type="text" name="inscricao_estadual" />
-                                </div>
-                            </div>
-                        </div>
+                        {
+                            modelo.map((linhas, i) => (
+                                <div key={i} className="row">
+                                    {
+                                        linhas.linha.map((item, j) => (
+                                            <div key={j} className={`col-sm-${item.width}`}>
+                                                <div className="form-group">
+                                                    <label>{item.descricao}</label>
+                                                    <Field className="form-control"
+                                                        placeholder={item.placeholder}
+                                                        type={item.type}
+                                                        name={item.name}
+                                                    />
+                                                </div>
+                                            </div>))
+                                    }
+                                </div>))
+                        }
+
                     </div>
                 </Form>
             </Formik>
         </div>
-
     )
 }
 export default FormEmpresa;
