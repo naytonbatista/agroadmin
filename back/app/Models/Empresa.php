8e4fb6f86d49b60247ac1fab24d6bf3b379aa0ef
Nayton Batista <naytonbatista@gmail.com>
2019-09-04 23:06:45 -0300

    alteração empresa model

--- back/app/Models/Empresa.php
+++ back/app/Models/Empresa.php
@@ -6,7 +6,13 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class Empresa extends Eloquent
 {
-    protected $fillable = ['razao_social', 'cnpj', 'inscricao_estadual', 'endereco', 'home_page'];
+    protected $fillable = ['razao_social', 
+                            'cnpj', 
+                            'inscricao_estadual', 
+                            'homepage', 
+                            'quantidade_empregados', 
+                            'socio_administrador', 
+                            'sede'];
 
     protected $table = 'empresa';
 }
